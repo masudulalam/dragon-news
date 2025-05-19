c709e092cd340a35bbbb8ab474b90e143536c741
masudulalam <masuudulalam@gmail.com>
2025-05-19 13:56:47 +0600

    added HomeLayout in layouts folder in src

---
+++ dragon-news/src/layouts/HomeLayout.jsx
@@ -0,0 +1,19 @@
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header>
+
+            </header>
+            <nav>
+                
+            </nav>
+            <main>
+
+            </main>
+        </div>
+    );
+};
+
+export default HomeLayout;
